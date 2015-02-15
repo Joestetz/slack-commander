27a5195d4deaa80406946a353bc9c4ae4eb04517
Joestetz <Joestetz@gmail.com>
2015-02-15 15:32:57 -0500

    added edit and help commands

--- server/api/task/task.controller.js
+++ server/api/task/task.controller.js
@@ -18,6 +18,9 @@ exports.index = function(req, res) {
     case 'add':
       commandResponse = cmdAdd(res, parsed);
       break;
+    case 'edit':
+      commandResponse = cmdEdit(res, parsed);
+      break;
     case 'complete':
       commandResponse = cmdComplete(res, parsed);
       break;
@@ -45,6 +48,9 @@ exports.index = function(req, res) {
     case 'unassigned':
       commandResponse = cmdUnassigned(res, parsed);
       break;
+    case 'help':
+      commandResponse = cmdHelp(res, parsed);
+      break;
     default:
       commandResponse = handleError(res, 'Command not recognized');
       break;
@@ -53,23 +59,11 @@ exports.index = function(req, res) {
   }
 };
 
-// function getWebhookConnection(token) {
-  // var hookURl;
-  // switch(token) {
-    // case 'm1PhkiEHawVYUtx6E7iyROt9':
-      // hookUrl = 'https://hooks.slack.com/services/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';
-      // break;
-    // default: break;
-  // }
-  
-  // return new Slack(hookUrl);
-// }
-
-function sendWebhook(commandObj, attachments) {
-  //var slack = getWebhookConnection(commandObj.token);
+function sendWebhook(commandObj, attachments, msg) {
   var options = {
     username: 'TaskerBot',
-    icon_emoji: 'pencil',
+    icon_emoji: ':pencil:',
+    text: 'Visit http://joestetz.com/showcase/slackCommander/tasker for more details',
     attachments: attachments
   };
   
@@ -77,7 +71,10 @@ function sendWebhook(commandObj, attachments) {
     options.channel = '@' + commandObj.user;
   }
   
-  //slack.send(options);
+  if(!!msg) {
+    options.text = msg;
+  }
+  
   Slack.send(options);
 }
 
@@ -181,6 +178,199 @@ function joinArgs(args, start) {
   return str;
 }
 
+function cmdHelp(res, commandObj) {
+  if(commandObj.command !== 'help') return handleError(res, 'Unexpected command');
+  
+  var attachments = [
+    {
+      fallback: 'Command: /tasker help - Displays list of available tasker commands',
+      text: 'Help',
+      fields: [
+        {
+          title: 'Usage',
+          value: '/tasker help',
+          'short': true
+        },{
+          title: 'Description',
+          value: 'Displays list of available tasker commands',
+          'short': true
+        }
+      ]
+    },{
+      fallback: 'Command: /tasker add [task] - Adds a task for your team',
+      text: 'Add Task',
+      fields: [
+        {
+          title: 'Usage',
+          value: '/tasker add [task]',
+          'short': true
+        },{
+          title: 'Description',
+          value: 'Adds a task for your team',
+          'short': true
+        }
+      ]
+    },{
+      fallback: 'Command: /tasker edit [task id] [task] - Edits the task name',
+      text: 'Edit Task',
+      fields: [
+        {
+          title: 'Usage',
+          value: '/tasker edit [task id] [task]',
+          'short': true
+        },{
+          title: 'Description',
+          value: 'Edits the task name',
+          'short': true
+        }
+      ]
+    },{
+      fallback: 'Command: /tasker complete [task id] - Marks the specified task as complete',
+      text: 'Edit Task',
+      fields: [
+        {
+          title: 'Usage',
+          value: '/tasker complete [task id]',
+          'short': true
+        },{
+          title: 'Description',
+          value: 'Marks the specified task as complete',
+          'short': true
+        }
+      ]
+    },{
+      fallback: 'Command: /tasker me [task id] - Assigns the specific task to you',
+      text: 'Grab Task',
+      fields: [
+        {
+          title: 'Usage',
+          value: '/tasker me [task id]',
+          'short': true
+        },{
+          title: 'Description',
+          value: 'Assigns the specific task to you',
+          'short': true
+        }
+      ]
+    },{
+      fallback: 'Command: /tasker assign [task id] [user name] - Assigns the specific task to the specified user. A blank user name is the same as the command',
+      text: 'Assign Task',
+      fields: [
+        {
+          title: 'Usage',
+          value: '/tasker assign [task id] [user name]',
+          'short': true
+        },{
+          title: 'Description',
+          value: 'Assigns the specific task to the specified user. A blank user name is the same as the me command',
+          'short': true
+        }
+      ]
+    },{
+      fallback: 'Command: /tasker delete [task id] - Deletes the specific task for your team',
+      text: 'Delete Task',
+      fields: [
+        {
+          title: 'Usage',
+          value: '/tasker delete [task id]',
+          'short': true
+        },{
+          title: 'Description',
+          value: 'Deletes the specific task for your team',
+          'short': true
+        }
+      ]
+    },{
+      fallback: 'Command: /tasker [shout] info [task id] - Displays information about a specific task',
+      text: 'Task Info',
+      fields: [
+        {
+          title: 'Usage',
+          value: '/tasker [shout] info [task id]',
+          'short': true
+        },{
+          title: 'Description',
+          value: 'Displays information about a specific task. If shout is added, it will broadcast to default channel.',
+          'short': true
+        }
+      ]
+    },{
+      fallback: 'Command: /tasker [shout] [list] - Displays a list of tasks for your team',
+      text: 'Show All Tasks',
+      fields: [
+        {
+          title: 'Usage',
+          value: '/tasker [shout] [list]',
+          'short': true
+        },{
+          title: 'Description',
+          value: 'Displays a list of tasks for your team. If shout is added, it will broadcast to default channel.',
+          'short': true
+        }
+      ]
+    },{
+      fallback: 'Command: /tasker [shout] done - Displays all completed tasks for your team',
+      text: 'Show Completed Tasks',
+      fields: [
+        {
+          title: 'Usage',
+          value: '/tasker [shout] done',
+          'short': true
+        },{
+          title: 'Description',
+          value: 'Displays all completed tasks for your team. If shout is added, it will broadcast to default channel.',
+          'short': true
+        }
+      ]
+    },{
+      fallback: 'Command: /tasker [shout] doing - Displays a list of tasks assigned to you',
+      text: 'Show My Tasks',
+      fields: [
+        {
+          title: 'Usage',
+          value: '/tasker [shout] doing',
+          'short': true
+        },{
+          title: 'Description',
+          value: 'Displays a list of tasks assigned to you. If shout is added, it will broadcast to default channel.',
+          'short': true
+        }
+      ]
+    },{
+      fallback: 'Command: /tasker [shout] assigned - Displays a list of in progress tasks',
+      text: 'Show Assigned Tasks',
+      fields: [
+        {
+          title: 'Usage',
+          value: '/tasker [shout] assigned',
+          'short': true
+        },{
+          title: 'Description',
+          value: 'Displays a list of in progress tasks. If shout is added, it will broadcast to default channel.',
+          'short': true
+        }
+      ]
+    },{
+      fallback: 'Command: /tasker [shout] unassigned - Displays a list of available tasks',
+      text: 'Show Assigned Tasks',
+      fields: [
+        {
+          title: 'Usage',
+          value: '/tasker [shout] unassigned',
+          'short': true
+        },{
+          title: 'Description',
+          value: 'Displays a list of available tasks. If shout is added, it will broadcast to default channel.',
+          'short': true
+        }
+      ]
+    }
+  ];
+  sendWebhook(commandObj, attachments, 'Usage: /tasker [shout] [command] [args1..N]');
+  
+  return res.send(200);
+}
+
 function cmdAdd(res, commandObj) {
   if(commandObj.command !== 'add') return handleError(res, 'Unexpected command');
   if(commandObj.commandArgs.length == 0) {
@@ -202,6 +392,26 @@ function cmdAdd(res, commandObj) {
   });
 }
     
+function cmdEdit(res, commandObj) {
+  if(commandObj.command !== 'edit') return handleError(res, 'Unexpected command');
+  if(commandObj.commandArgs.length == 0) {
+    return handleError(res, 'You must specify a task id');
+  }
+  
+  Task.findById(commandObj.commandArgs[0], function (err, task) {
+    if(err) { return handleError(res, err); }
+    if(!task || task.token !== commandObj.token || task.teamId !== commandObj.teamId) {
+      return handleError(res, 'Task not found');
+    }
+    
+    task.name = joinArgs(commandObj.commandArgs, 1);
+    task.save(function (err) {
+      if (err) { return handleError(res, err); }
+      return handleSuccess(res, 'Task updated');
+    });
+  });
+}
+  
 function cmdComplete(res, commandObj) {
   if(commandObj.command !== 'complete') return handleError(res, 'Unexpected command');
   if(commandObj.commandArgs.length == 0) {
@@ -358,4 +568,7 @@ function cmdUnassigned(res, commandObj) {
     
     return res.send(200);
   }); 
+}
+
+function cmdHelp(res, commandObj) {
 }
